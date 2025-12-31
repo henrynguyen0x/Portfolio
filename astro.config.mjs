47de9ee0aa8f3e80a4e2bf14e16b6f34f1a61e62
henrynguyen0x <vanhieunguyen.vn@gmail.com>
2025-12-31 00:20:58 +0000

    Add new astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-// https://astro.build/config
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
+  output: "static",
+  site: "https://henrynguyen0x.github.io",
+  base: "/Portfolio",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
